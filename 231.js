ff669bdea258ba1dccd75ffc1162b7bf4d3322cb
alwinabraham <alwinabraham098@gmail.com>
2023-06-27 23:54:52 +0530

    Power of four

--- 231.js
+++ 231.js
@@ -1,6 +1,5 @@
 var isPowerOfTwo = function(n) {
-    // return (Math.log(n)/Math.log(2)).toFixed(10) % 1 == 0
-    return Math.log2(n) % 1 ===0 ? true : false
+    return (Math.log(n)/Math.log(2)).toFixed(10) % 1 == 0
 };
 
 console.log(isPowerOfTwo(16))
---
+++ 342.js
@@ -0,0 +1,6 @@
+var isPowerOfFour = function(n) {
+    console.log(Math.log(n)/Math.log(4)%1 == 0);
+    console.log(Math.log(n)/Math.log(4) % 1 == 0)
+};
+
+isPowerOfFour(16)
