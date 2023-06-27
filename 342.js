265c60f730970d1926fda2bd46cc68e205cd96d3
alwinabraham <alwinabraham098@gmail.com>
2023-06-27 23:58:45 +0530

    Power of four return

--- 342.js
+++ 342.js
@@ -1,6 +1,5 @@
 var isPowerOfFour = function(n) {
-    console.log(Math.log(n)/Math.log(4)%1 == 0);
-    console.log(Math.log(n)/Math.log(4) % 1 == 0)
+    return (Math.log(n)/Math.log(4) % 1 == 0)
 };
 
 isPowerOfFour(16)
